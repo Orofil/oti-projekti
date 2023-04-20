becacff8b3eb49cfa922bddc58e3b42651f561d2
Orofil <saano.jaakko@gmail.com>
2023-04-20 18:03:51 +0300

    Tiiviimpi toteutus asettelulle

--- src/main/java/com/example/otiprojekti/Main.java
+++ src/main/java/com/example/otiprojekti/Main.java
@@ -19,6 +19,9 @@ import javafx.scene.text.Font;
 
 
 public class Main extends Application {
+    /**
+     * Polku projektin resurssikansioon.
+     */
     private static final String IMGPOLKU = "src/main/resources/com/example/otiprojekti/";
 
     @Override
@@ -27,10 +30,13 @@ public class Main extends Application {
 
         VBox painikkeet = new VBox();
 
+        // TODO näiden mittojen pitää mukautua ikkunan kokoon jos se muuttuu näytön koon mukaan
         Rectangle palkki = new Rectangle(220, 100, Color.BLACK);
         Rectangle palkki2 = new Rectangle(220, 500, Color.BLACK);
 
-        // TODO nämä nappulat voisi tehdä loopilla jostain listasta jossa vain lukee nuo nappien tekstit, esim seuraavanlaisesta
+        painikkeet.getChildren().add(palkki);
+
+        // Määritellään tekstit sivuvalikolle
         String[] nakymaTekstit = new String[] {
                 "Alueet",
                 "Mökit",
@@ -40,98 +46,30 @@ public class Main extends Application {
                 "Laskut"
         };
 
-        Rectangle aluenappula = new Rectangle(219, 40, Color.BLACK);
-        aluenappula.setStroke(Color.BLACK);
-        Label alueteksti = new Label("Alueet", aluenappula);
-        alueteksti.setContentDisplay(ContentDisplay.CENTER);
-        alueteksti.setTextFill(Color.WHITE);
-        alueteksti.setFont(Font.font(18));
-        alueteksti.setOnMouseEntered( e -> {
-            aluenappula.setStroke(Color.WHITE);
-        });
-        alueteksti.setOnMouseExited( e -> {
-            aluenappula.setStroke(Color.BLACK);
-        });
+        // Luodaan sivuvalikon painikkeet
+        for (String teksti : nakymaTekstit) {
+            Rectangle nappula = new Rectangle(219, 40, Color.BLACK);
+            nappula.setStroke(Color.BLACK);
 
-        Rectangle mokkinappula = new Rectangle(219, 40, Color.BLACK);
-        mokkinappula.setStroke(Color.BLACK);
-        Label mokkiteksti = new Label("Mökit", mokkinappula);
-        mokkiteksti.setContentDisplay(ContentDisplay.CENTER);
-        mokkiteksti.setTextFill(Color.WHITE);
-        mokkiteksti.setFont(Font.font(18));
-        mokkiteksti.setOnMouseEntered( e -> {
-            mokkinappula.setStroke(Color.WHITE);
-        });
-        mokkiteksti.setOnMouseExited( e -> {
-            mokkinappula.setStroke(Color.BLACK);
-        });
+            Label label = new Label(teksti, nappula);
+            label.setContentDisplay(ContentDisplay.CENTER);
+            label.setTextFill(Color.WHITE);
+            label.setFont(Font.font(18));
+            label.setOnMouseEntered( e -> nappula.setStroke(Color.WHITE));
+            label.setOnMouseExited( e -> nappula.setStroke(Color.BLACK));
 
-        Rectangle palvelunappula = new Rectangle(219, 40, Color.BLACK);
-        palvelunappula.setStroke(Color.BLACK);
-        Label palveluteksti = new Label("Palvelut", palvelunappula);
-        palveluteksti.setContentDisplay(ContentDisplay.CENTER);
-        palveluteksti.setTextFill(Color.WHITE);
-        palveluteksti.setFont(Font.font(18));
-        palveluteksti.setOnMouseEntered( e -> {
-            palvelunappula.setStroke(Color.WHITE);
-        });
-        palveluteksti.setOnMouseExited( e -> {
-            palvelunappula.setStroke(Color.BLACK);
-        });
+            painikkeet.getChildren().add(label);
+        }
 
-        Rectangle varausnappula = new Rectangle(219, 40, Color.BLACK);
-        varausnappula.setStroke(Color.BLACK);
-        Label varausteksti = new Label("Varaukset", varausnappula);
-        varausteksti.setContentDisplay(ContentDisplay.CENTER);
-        varausteksti.setTextFill(Color.WHITE);
-        varausteksti.setFont(Font.font(18));
-        varausteksti.setOnMouseEntered( e -> {
-            varausnappula.setStroke(Color.WHITE);
-        });
-        varausteksti.setOnMouseExited( e -> {
-            varausnappula.setStroke(Color.BLACK);
-        });
-
-
-        Rectangle asiakasnappula = new Rectangle(219, 40, Color.BLACK);
-        asiakasnappula.setStroke(Color.BLACK);
-        Label asiakasteksti = new Label("Asiakkaat", asiakasnappula);
-        asiakasteksti.setContentDisplay(ContentDisplay.CENTER);
-        asiakasteksti.setTextFill(Color.WHITE);
-        asiakasteksti.setFont(Font.font(18));
-        asiakasteksti.setOnMouseEntered( e -> {
-            asiakasnappula.setStroke(Color.WHITE);
-        });
-        asiakasteksti.setOnMouseExited( e -> {
-            asiakasnappula.setStroke(Color.BLACK);
-        });
-
-
-        Rectangle laskunappula = new Rectangle(219, 40, Color.BLACK);
-        laskunappula.setStroke(Color.BLACK);
-        Label laskuteksti = new Label("Laskut", laskunappula);
-        laskuteksti.setContentDisplay(ContentDisplay.CENTER);
-        laskuteksti.setTextFill(Color.WHITE);
-        laskuteksti.setFont(Font.font(18));
-        laskuteksti.setOnMouseEntered( e -> {
-            laskunappula.setStroke(Color.WHITE);
-        });
-        laskuteksti.setOnMouseExited( e -> {
-            laskunappula.setStroke(Color.BLACK);
-        });
-
-
-        painikkeet.getChildren().addAll
-                (palkki, alueteksti, mokkiteksti, palveluteksti, varausteksti, asiakasteksti, laskuteksti, palkki2);
-
-
+        painikkeet.getChildren().add(palkki2);
 
         paneeli.setLeft(painikkeet);
 
+        // TODO tälle "laastariratkaisulle" jokin parempi tapa, kuten erillisessä luokassa oleva julkinen metodi kuvan avaamiseen
         try {
             Image ylapalkinkuva = new Image(IMGPOLKU + "ylapalkki.png");
             ImageView ylapalkki = new ImageView(ylapalkinkuva);
-            ylapalkki.setFitWidth(1600);
+            ylapalkki.setFitWidth(1600); // TODO näiden mittojen pitää mukautua ikkunan kokoon jos se muuttuu näytön koon mukaan
             ylapalkki.setFitHeight(100);
             paneeli.setTop(ylapalkki);
         } catch (Exception e) {
@@ -150,7 +88,7 @@ public class Main extends Application {
 
         GridPane alueTaulukko = new GridPane();
         alueScrollaus.setContent(alueTaulukko);
-        alueTaulukko.add(new Rectangle(350, 350), 0, 0);
+        alueTaulukko.add(new Rectangle(350, 350), 0, 0); // TEMP
         alueTaulukko.add(new Rectangle(350, 350), 1, 1);
         alueTaulukko.add(new Rectangle(350, 350), 0, 2);
         alueTaulukko.add(new Rectangle(350, 350), 1, 3);
