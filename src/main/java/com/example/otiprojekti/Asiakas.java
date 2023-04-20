59809e550c187ad3f8b8dbb9dc1aa9df2c505ab9
Orofil <saano.jaakko@gmail.com>
2023-04-20 10:51:18 +0300

    Lisätty varaus, asiakas ja SQL

---
+++ src/main/java/com/example/otiprojekti/Asiakas.java
@@ -0,0 +1,104 @@
+package com.example.otiprojekti;
+
+import java.util.Objects;
+
+public class Asiakas {
+    private int asiakasID;
+    private int postinro;
+
+    private String sukunimi;
+    private String etunimi;
+    private String email;
+    private String lahiosoite;
+    private String puhnro;
+
+    public Asiakas(int asiakasID,
+                   int postinro,
+                   String sukunimi,
+                   String etunimi,
+                   String email,
+                   String lahiosoite,
+                   String puhnro) {
+        this.asiakasID = asiakasID;
+        this.postinro = postinro;
+
+        this.sukunimi = sukunimi;
+        this.etunimi = etunimi;
+        this.email = email;
+        this.lahiosoite = lahiosoite;
+        this.puhnro = puhnro;
+    }
+
+    public int getAsiakasID() {
+        return asiakasID;
+    }
+
+    public int getPostinro() {
+        return postinro;
+    }
+
+    public void setPostinro(int postinro) {
+        this.postinro = postinro;
+    }
+
+    public String getSukunimi() {
+        return sukunimi;
+    }
+
+    public void setSukunimi(String sukunimi) {
+        this.sukunimi = sukunimi;
+    }
+
+    public String getEtunimi() {
+        return etunimi;
+    }
+
+    public void setEtunimi(String etunimi) {
+        this.etunimi = etunimi;
+    }
+
+    /**
+     * @return Asiakkaan etu- ja sukunimi
+     */
+    public String getNimi() {
+        return etunimi + " " + sukunimi;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLahiosoite() {
+        return lahiosoite;
+    }
+
+    public void setLahiosoite(String lahiosoite) {
+        this.lahiosoite = lahiosoite;
+    }
+
+    public String getPuhnro() {
+        return puhnro;
+    }
+
+    public void setPuhnro(String puhnro) {
+        this.puhnro = puhnro;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Asiakas asiakas = (Asiakas) o;
+        return asiakasID == asiakas.asiakasID &&
+                postinro == asiakas.postinro &&
+                Objects.equals(sukunimi, asiakas.sukunimi) &&
+                Objects.equals(etunimi, asiakas.etunimi) &&
+                Objects.equals(email, asiakas.email) &&
+                Objects.equals(lahiosoite, asiakas.lahiosoite) &&
+                Objects.equals(puhnro, asiakas.puhnro);
+    }
+}
---
+++ src/main/java/com/example/otiprojekti/Varaus.java
@@ -0,0 +1,109 @@
+package com.example.otiprojekti;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Varaus {
+    private int varausID;
+    private int asiakasID;
+    private int mokkiID;
+
+    private LocalDateTime varattuPvm;
+    private LocalDateTime vahvistusPvm;
+    private LocalDateTime varausAlkuPvm;
+    private LocalDateTime varausLoppuPvm;
+
+    public Varaus(int varausID,
+                  int asiakasID,
+                  int mokkiID,
+                  LocalDateTime varattuPvm,
+                  LocalDateTime vahvistusPvm,
+                  LocalDateTime varausAlkuPvm,
+                  LocalDateTime varausLoppuPvm) {
+        this.varausID = varausID;
+        this.asiakasID = asiakasID;
+        this.mokkiID = mokkiID;
+
+        this.varattuPvm = varattuPvm;
+        this.vahvistusPvm = vahvistusPvm;
+        this.varausAlkuPvm = varausAlkuPvm;
+        this.varausLoppuPvm = varausLoppuPvm;
+    }
+
+    public int getVarausID() {
+        return varausID;
+    }
+
+    public int getAsiakasID() {
+        return asiakasID;
+    }
+
+    public void setAsiakasID(int asiakasID) {
+        this.asiakasID = asiakasID;
+    }
+
+    public int getMokkiID() {
+        return mokkiID;
+    }
+
+    public void setMokkiID(int mokkiID) {
+        this.mokkiID = mokkiID;
+    }
+
+    public LocalDateTime getVarattuPvm() {
+        return varattuPvm;
+    }
+
+    public void setVarattuPvm(LocalDateTime varattuPvm) {
+        this.varattuPvm = varattuPvm;
+    }
+
+    public LocalDateTime getVahvistusPvm() {
+        return vahvistusPvm;
+    }
+
+    public void setVahvistusPvm(LocalDateTime vahvistusPvm) {
+        this.vahvistusPvm = vahvistusPvm;
+    }
+
+    public LocalDateTime getVarausAlkuPvm() {
+        return varausAlkuPvm;
+    }
+
+    public void setVarausAlkuPvm(LocalDateTime varausAlkuPvm) {
+        this.varausAlkuPvm = varausAlkuPvm;
+    }
+
+    public LocalDateTime getVarausLoppuPvm() {
+        return varausLoppuPvm;
+    }
+
+    public void setVarausLoppuPvm(LocalDateTime varausLoppuPvm) {
+        this.varausLoppuPvm = varausLoppuPvm;
+    }
+
+    @Override
+    public String toString() {
+        return "Varaus[" +
+                "varausID=" + varausID +
+                ", mokkiID=" + mokkiID +
+                ", varattuPvm=" + varattuPvm +
+                ", varausAlkuPvm=" + varausAlkuPvm +
+                ", varausLoppuPvm=" + varausLoppuPvm +
+                "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Varaus varaus = (Varaus) o;
+        return varausID == varaus.varausID &&
+                asiakasID == varaus.asiakasID &&
+                mokkiID == varaus.mokkiID &&
+                Objects.equals(varattuPvm, varaus.varattuPvm) &&
+                Objects.equals(vahvistusPvm, varaus.vahvistusPvm) &&
+                Objects.equals(varausAlkuPvm, varaus.varausAlkuPvm) &&
+                Objects.equals(varausLoppuPvm, varaus.varausLoppuPvm);
+    }
+}
