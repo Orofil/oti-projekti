72e8dd01a4ae9e6f4f949d38d22d30580eada9a6
Orofil <saano.jaakko@gmail.com>
2023-05-01 10:50:00 +0300

    TODO pois

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.otiprojekti {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires mysql.connector.j; // TODO miten tämä ulkoisen kirjaston vaatimus toteutetaan, SQL yhdistäminen ei toimi ilman tätä
+    requires mysql.connector.j; // MySQL:n yhdistämiseen tarvittava moduuli
 
     opens com.example.otiprojekti to javafx.fxml;
     exports com.example.otiprojekti;
