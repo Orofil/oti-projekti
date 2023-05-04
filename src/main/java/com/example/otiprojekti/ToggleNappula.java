6819c1df2eff62cfd03a79428d78768dcc66575a
Orofil <saano.jaakko@gmail.com>
2023-05-04 16:07:40 +0300

    TODO päivitys

--- src/main/java/com/example/otiprojekti/ToggleNappula.java
+++ src/main/java/com/example/otiprojekti/ToggleNappula.java
@@ -14,7 +14,7 @@ public class ToggleNappula extends ToggleButton {
         setFont(Font.font(18));
         setTextFill(Color.WHITE);
         // TODO näille erillinen stylesheet
-        // TODO tämä ei vielä eroa visuaalisesti normaalista nappulasta
+        // TODO tämä ei vielä eroa visuaalisesti normaalista nappulasta, pitäisi saada muuttamaan väriä kun on valittuna
         setStyle("-fx-background-radius: 0; -fx-background-color: black; -fx-border-color: black;");
         setOnMouseEntered(e -> setStyle("-fx-background-radius: 0; -fx-background-color: black; -fx-border-color: white;"));
         setOnMouseExited(e -> setStyle("-fx-background-radius: 0; -fx-background-color: black; -fx-border-color: black;"));
