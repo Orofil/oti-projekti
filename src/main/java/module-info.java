eca182eb1808768df4ebae6867df136ddbfa73ee
veikkakukko <veikka.kukko@gmail.com>
2023-05-09 12:33:36 +0300

    itext pois

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,9 @@ module com.example.otiprojekti {
     requires javafx.fxml;
     requires java.sql;
     requires mysql.connector.j;
-    //requires itextpdf;
+    requires itextpdf;
     requires java.desktop;
-    requires itext;
+    //requires itext;
 
     opens com.example.otiprojekti to javafx.fxml;
     exports com.example.otiprojekti;
