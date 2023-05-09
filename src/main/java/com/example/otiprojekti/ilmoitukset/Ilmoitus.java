609cc3c93c27148cd39921d709b2a582e05e6483
Orofil <saano.jaakko@gmail.com>
2023-05-09 13:40:16 +0300

    Ilmoituksen tekstin lyhentäminen

--- src/main/java/com/example/otiprojekti/ilmoitukset/Ilmoitus.java
+++ src/main/java/com/example/otiprojekti/ilmoitukset/Ilmoitus.java
@@ -12,18 +12,25 @@ public class Ilmoitus extends StackPane {
 
     private Rectangle rect;
     private Text text;
+    private String teksti;
     private int kesto;
 
     public Ilmoitus(IlmoitusTyyppi tyyppi, String teksti, int kesto) {
         super();
 
         rect = new Rectangle(leveys, korkeus);
+
+        this.teksti = teksti;
+        // Lyhennetään teksti jos se on liian pitkä
+        if (teksti.length() > 72) {
+            teksti = teksti.substring(0, 71) + "...";
+        }
         text = new Text(teksti);
 
         rect.setArcWidth(kulmaSade);
         rect.setArcHeight(kulmaSade);
         text.setWrappingWidth(leveys - 10);
-        // Teksti voi olla liian pitkä, ratkaisuna voi olla https://stackoverflow.com/questions/65715608/truncating-text-after-a-specific-number-of-lines-in-javafx mutta meneekö vähän liian monimutkaiseksi
+
 
         Color vari = null;
         switch (tyyppi) {
